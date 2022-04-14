d0919975fe2990ad3662aecc6fe9aac439a6e3df
wanghai <wanghai@133.cn>
2022-04-14 18:28:35 +0800

    新增untitled2

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Untitled from '@/components/Untitled'
+import Untitled2 from '@/components/Untitled2'
 import People from '@/components/People'
 
 Vue.use(Router)
@@ -17,6 +18,11 @@ export default new Router({
       path: '/Untitled',
       name: 'Untitled',
       component: Untitled
+    },
+    {
+      path: '/Untitled2',
+      name: 'Untitled2',
+      component: Untitled2
     },
     {
       path: '/People',
